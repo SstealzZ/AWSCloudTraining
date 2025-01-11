53c76dd6d0a2fcc973be63202103e0cd22bd31b4
SstealzZ <antoine.veber@epitech.eu>
2025-01-11 13:48:45 +0100

    Modify app.py - ADD CORS Policies

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
@@ -25,6 +26,15 @@ Base.metadata.create_all(bind=engine)
 
 # API setup
 app = FastAPI()
+
+# Configuration CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class NoteCreate(BaseModel):
     title: str
